2a99a298242086680083ad96c3c7f64d72a6a904
aunjum <aunjum.uxd.co.jp>
2022-09-22 15:45:15 +0600

    rocket-mongodb-rest-api added

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,77 @@
-use chrono::{TimeZone, Utc};
-use mongodb::bson::doc;
-use mongodb::{
-    options::{ClientOptions, ResolverConfig},
-    Client,
+#[macro_use]
+extern crate rocket;
+
+use rocket::http::Method;
+use rocket::http::Status;
+use rocket::serde::json::Json;
+use rocket_cors::{AllowedOrigins, CorsOptions};
+
+use crate::constants::{UNAUTHORIZED, UNKNOWN};
+use crate::database::connect_to_db::init;
+use crate::error_response::error_responses::{
+    ErrorResponse, NOT_FOUND_JSON, UNAUTHORIZED_JSON, UNKNOWN_JSON,
 };
-use std::env;
-use std::error::Error;
-use std::time::Instant;
+use crate::helper::check_valid_text;
+use crate::routes::authorization::login::login;
+use crate::routes::authorization::registration::registration;
+use crate::routes::routes::delete_user::delete_user;
+use crate::routes::routes::get_data_user::get_data_user;
+use crate::routes::routes::hello_name::{hello_name_user, hello_world};
+use crate::routes::routes::patch_user::edit_user;
+use crate::routes::routes::refresh_tokens::refresh_tokens;
 
-use tokio;
-//use async_std;
+pub mod constants;
+mod database;
+pub mod error_response;
+mod helper;
+mod models;
+mod private;
+mod routes;
 
-// #[async_std::main]
-#[tokio::main]
-
-async fn main() -> Result<(), Box<dyn Error>> {
-    // set environment variable:
-    env::set_var("MONGODB_URI", "mongodb+srv://aunjum:u9t8vOivNEzL6I4y@cluster0.nmuah3o.mongodb.net/?retryWrites=true&w=majority");
-
-    // Load the mongodb connection string from the environment variable:
-    let client_uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set");
-
-    // A client is needed to work connect to MongoDB
-    // An extra line of code to work around a DNS issue on windows:
-    let options =
-        ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
-            .await?;
-    let client = Client::with_options(options)?;
-
-    // print the database in our mongodb cluster:
-    println!("Databases:");
-    let start = Instant::now();
-    for db_name in client.list_database_names(None, None).await? {
-        println!("{}", db_name);
-    }
-    let duration = start.elapsed();
-    println!("Time elapsed in get database list is: {:?}", duration);
-
-    //define new document model:
-    let new_doc = doc! {
-        "title": "Parasite",
-        "year": 2020,
-        "plot": "A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks.
-        But their easy life gets complicated when their deception is threatened with exposure.",
-        "released": Utc.ymd(2020, 2, 7).and_hms(0, 0, 0),
-    };
-    println!("Defined new doc: {:?}", new_doc);
-
-    //insert new document:
-    let movies = client.database("sample_mflix").collection("movies");
-    let insert_result = movies.insert_one(new_doc.clone(), None).await?;
-    println!("Inserted document: {:?}", insert_result);
-
-    // Look for the document we just inserted:
-    let movie = movies
-        .find_one(Some(doc! { "title": "Parasite" }), None)
-        .await?
-        .expect("Missing 'Parasite' document");
-    println!("Found document: {:?}", movie);
-
-    //update the document:
-    let update_result = movies
-        .update_one(
-            doc! { "_id": &movie.get("_id") },
-            doc! { "$set": { "year": 2018 } },
-            None,
+#[launch]
+async fn rocket() -> _ {
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_methods(
+            vec![Method::Get, Method::Post, Method::Patch, Method::Delete]
+                .into_iter()
+                .map(From::from)
+                .collect(),
         )
-        .await?;
-    println!("Updated document: {:?}", update_result.modified_count);
+        .allow_credentials(true);
+    rocket::build()
+        .attach(init().await)
+        .mount(
+            "/api/v1",
+            routes![
+                registration,
+                login,
+                hello_name_user,
+                hello_world,
+                refresh_tokens,
+                delete_user,
+                edit_user,
+                get_data_user
+            ],
+        )
+        .manage(cors.to_cors())
+        .register(
+            "/",
+            catchers![unauthorized, not_found, internal_sever_error,],
+        )
+}
 
-    let movie = movies
-        .find_one(Some(doc! { "_id": &movie.get("_id") }), None)
-        .await?
-        .expect("Missing 'Parasite' document");
-    println!("Updated Movie: {:?}", &movie);
+#[catch(401)]
+pub fn unauthorized() -> Json<ErrorResponse> {
+    Json(UNAUTHORIZED_JSON)
+}
 
-    //Delete all documents for movies called "Parasite":
-    let delete_result = movies
-        .delete_many(doc! { "title": "Parasite" }, None)
-        .await?;
-    println!("Deleted documents: {:?}", delete_result.deleted_count);
+#[catch(404)]
+pub fn not_found() -> Json<ErrorResponse> {
+    Json(NOT_FOUND_JSON)
+}
 
-
-    Ok(())
+#[catch(500)]
+pub fn internal_sever_error() -> Json<ErrorResponse> {
+    Json(UNKNOWN_JSON)
 }
---
+++ src/private.rs
@@ -0,0 +1,3 @@
+pub const JWT_SECRET: &'static str = "y/B?E(H+MbQeShVmYq3t6w9z$C&F)J@N";
+pub const REFRESH_JWT_SECRET: &'static str = "A?D(G+KaPdSgVkYp3s6v9y$B&E)H@McQ";
+pub(crate) const URL_DB: &str = "mongodb+srv://aunjum:u9t8vOivNEzL6I4y@cluster0.nmuah3o.mongodb.net/rust?retryWrites=true&w=majority";
